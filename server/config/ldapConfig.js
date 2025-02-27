051896ab56dda04885ef802042fd3a2c4dcc677d
drbn68 <emberizacirlus@yahoo.es>
2025-02-27 15:46:27 +0100

    ldap

--- server/config/ldapConfig.js
+++ server/config/ldapConfig.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  url: process.env.LDAP_URL || 'ldap://your-ldap-server:389',
-  baseDN: process.env.LDAP_BASE_DN || 'dc=example,dc=com',
-  bindDN: process.env.LDAP_BIND_DN || 'cn=admin,dc=example,dc=com',
-  bindCredentials: process.env.LDAP_BIND_CREDENTIALS || 'admin_password'
+  url: process.env.LDAP_URL || 'ldap://192.168.100.68:389',
+  baseDN: process.env.LDAP_BASE_DN || 'dc=fp,dc=local',
+  bindDN: process.env.LDAP_BIND_DN || 'uid=binduser,cn=users,dc=fundacio-puigvert,dc=com',
+  bindCredentials: process.env.LDAP_BIND_CREDENTIALS || 'j15V$I62V48jxI7'
 };
